6d75802781543fe5786ca11d832edb8b522523b9
Shirozu <231205702@ccmailg.meijo-u.ac.jp>
2025-08-27 23:52:39 +0900

    フッターの更新

--- src/app/games/scout-crush/page.tsx
+++ src/app/games/scout-crush/page.tsx
@@ -73,9 +73,10 @@ function formatTime(ms: number) {
 
 function shuffleArray<T>(arr: T[]) {
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = (Math.random() * (i + 1)) | 0;
-    [arr[i], arr[j]] = [arr[j]], arr[i] = arr[i];
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    const tmp = arr[i]!;
+    arr[i] = arr[j]!;
+    arr[j] = tmp;
   }
 }
 
